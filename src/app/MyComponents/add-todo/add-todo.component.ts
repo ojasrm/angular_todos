90d3f7253ce31fbd042553de21f60ad610db5cc6
Ojas Mahajan <65729897+ojasrm@users.noreply.github.com>
2022-03-02 12:57:22 +0530

    Initial commit

---
+++ src/app/MyComponents/add-todo/add-todo.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { Todo } from "./../../Todo";
+
+@Component({
+  selector: "app-add-todo",
+  templateUrl: "./add-todo.component.html",
+  styleUrls: ["./add-todo.component.css"]
+})
+export class AddTodoComponent implements OnInit {
+  title: string;
+  desc: string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {}
+  onSubmit() {
+    const todo = {
+      sno: 8,
+      title: this.title,
+      desc: this.desc,
+      active: true
+    };
+    this.todoAdd.emit(todo);
+  }
+}
---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
+import { Todo } from "./../../Todo";
+
+@Component({
+  selector: "app-todo-item",
+  templateUrl: "./todo-item.component.html",
+  styleUrls: ["./todo-item.component.css"]
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo: Todo;
+  @Input() i: number;
+  @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
+  constructor() {}
+
+  ngOnInit(): void {}
+  onClick(todo: Todo) {
+    this.todoDelete.emit(todo);
+    console.log("Onclick triggered");
+  }
+  onCheckboxClick(todo) {
+    this.todoCheckbox.emit(todo);
+  }
+}
